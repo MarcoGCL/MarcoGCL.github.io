4494394720c6de080d763d2ab92c8af2eae382eb
Marco Giancola <marcogiancola95@gmail.com>
2019-07-07 08:49:17 -0400

    Add files via upload

---
+++ build/precache-manifest.a98eef2853646278d92d48db12af7025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b4fe533faf0c7eb4f2fff1f3fde3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f036647dd7d241781db4",
+    "url": "/static/css/main.335d5283.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "f036647dd7d241781db4",
+    "url": "/static/js/main.38f8b81a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
